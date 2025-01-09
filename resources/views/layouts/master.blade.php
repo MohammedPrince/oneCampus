72706bb4afd763b1135d2e009a3145ff91d7f6bf
MohammedPrince <mohammed.beng@gmail.com>
2025-01-09 15:29:56 +0200

    Code updates.Set the login page in Main

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
         rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>One Campus::DEV</title>
+    <title>One Campus</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="./public/css/systemlogin.css">
