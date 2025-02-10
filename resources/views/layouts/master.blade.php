bd9243298e341e79164cdac63cc233b2763fd136
MohammedPrince <mohammed.beng@gmail.com>
2025-02-10 15:15:14 +0200

    Code updates. HTML,Routes and CSS fixes V05

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OneCampus</title>
+    <title>OneCampus::In Development</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/logo.svg') }}">
-    <link href="./public/css/bootstrap.min.css'" rel="stylesheet">
+    <link href="/public/css/bootstrap.min.css'" rel="stylesheet">
     <link href="{{ url('css/bootstrap-icons.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ url('css/main.css') }}">
 </head>
